0c95060d02492bd3a83ec14deedf58b1d234c62e
Husayn <husayn.mohammadi@outlook.com>
2024-03-18 11:27:09 +0330

    design: passed to inputData.py

--- design.py
+++ design.py
@@ -13,165 +13,30 @@ if recordToLogDesign == True:
 #                   Step 1: Input Data
 #||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||
 
-# Seismic Coefficients
-# Cd      = 5.5
-# Ie      = 1.
-# R       = 8
-# Omega0  = 2.5
-# Rho     = 1.
+        print("Section is under tension.")    
+    C = ((0.85 *Rc  *fpc *tc *tf +2 *tw *Ry *Fy *h -P)/
+         (0.85 *Rc  *fpc *tc     +4 *tw *Ry *Fy))
+    if C < 0:
+        print(f"C = {C:.3f} < 0\nThe program exits in calc_C()!!!"); sys.exit()
+    if C > h:
+        print(f"C = {C:.3f} > h = {h:.3f}\nThe program exits in calc_C()!!!"); sys.exit()
+    return C
 
-# Composite wall resistance factor
-# Fi_v = Fi_b = Fi_c = Fi_t = 0.9
+def calc_Mp(fpc, Fy, tc, tw, tf, h, bf, P=0, Ry=1, Rc=1):
+    y       = calc_C(fpc, Fy, tc, tw, tf, h, P, Ry, Rc)
+    C1 = T1 = bf *tf            *Ry *Fy
+    C2      = 2 *tw *(y -tf)    *Ry *Fy
+    C3      = tc    *(y -tf)    *Rc *(0.85 *fpc)
+    T2      = 2 *tw *(h -y -tf) *Ry *Fy
 
-# Building Geometry
-# Lf      = 200   *ft
-# Wf      = 120   *ft
-# h_typ   = 14    *ft
-# h_1     = 17    *ft
-# n_story = 10
+    Mp = ( C1 *( y -tf     /2) 
+          +C2 *((y -tf)    /2) 
+          +C3 *((y -tf)    /2) 
+          +T1 *( h -y -tf  /2) 
+          +T2 *((h -y -tf) /2) 
+          -P *(h /2 -y))
+    return Mp
 
-# Floor Loads
-# DL      = 0.12  *ksf
-# LL      = 0
-
-# Material
-# Es      = 29000 *ksi
-# Gs      = 11200 *ksi
-# Fy      = 50    *ksi
-# Fu      = 65    *ksi
-# Ry      = 1.1
-
-# fpc     = 6     *ksi
-# Ec      = 4500  *ksi
-# Gc      = 1800  *ksi
-# Rc      = 1.3
-
-# Wall and Coupling Beam Sections
-# Lw      = 132   *inch
-# tw      = 9/16  *inch
-# t_sc    = 24    *inch
-# btie    = 12    *inch
-# Stie    = 12    *inch
-# dtie    = 1     *inch
-
-# L_CB    = 96    *inch
-# b_CB    = 24    *inch
-# h_CB    = 24    *inch
-# t_pfCB  = 0.5   *inch
-# t_pwCB  = 3/8   *inch
-
-
-# Coupling Beam Properties
-# As_CB   = 2 *t_pwCB *h_CB + 2 *t_pfCB *(b_CB -2 *t_pwCB)
-# Ac_CB   = (b_CB -2 *t_pwCB) *(h_CB -2 *t_pfCB)
-# Asw_CB  = 2 *h_CB *t_pwCB
-# Is_CB   = 2 *(1/12 *t_pwCB *h_CB **3 + 1/12 *(b_CB -2 *t_pwCB) *t_pfCB **3 +
-#             t_pfCB *(b_CB -2 *t_pwCB) *((h_CB -t_pfCB) /2) **2)
-# Ic_CB   = 1/12 *(b_CB -2 *t_pwCB) *(h_CB -2 *t_pfCB) **3
-
-# EAuncrCB= 0.8 *(Es *As_CB +Ec *Ac_CB)
-# C3      = min(0.9, 0.45 +3 *(As_CB /b_CB/ h_CB))
-# EIeff_CB= 0.64 *(Es *Is_CB +C3 *Ec *Ic_CB)
-# GAv_CB  = Gs *Asw_CB + Gc *Ac_CB
-
-# Planar SpeedCore Wall Properties
-# As      = tw *(2 *(Lw -2 *tw) + 2 *t_sc)
-# Ac      = Lw *t_sc -As
-# Asw     = 2 *Lw *tw
-# Is      = (2 *(1/12 *tw *(Lw -2 *tw) **3) + 
-#            2 *(1/12 *t_sc *tw **3 + t_sc*tw *((Lw -tw) /2) **2))
-# Ic      = 1/12 * (t_sc -2 *tw) * (Lw -2 *tw) **3
-
-# EAeff   = Es *As  +0.45 *Ec *Ac
-# EIeff   = Es *Is  +0.35 *Ec *Ic
-# GAveff  = Gs *Asw +      Gc *Ac
-
-#______________________________________________________________________________
-#                   Check with Thresholds
-"""'''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''"""
-
-# 1.    Check Minimum and Maximum Area of Steel
-"""'''''''''''''''''''''''''''''''''''''''''"""
-# rho_min  = 0.01
-# rho_max  = 0.1
-
-# 1.1.  Coupling Beams
-# As_CBmin = rho_min *b_CB *h_CB
-# As_CBmax = rho_max *b_CB *h_CB
-# if As_CB < As_CBmin: 
-#     print("Area of Steel is less than minimum for Coupling Beams!!!")
-# elif As_CB > As_CBmax:
-#     print("Area of Steel is greater than maximum for Coupling Beams!!!")
-
-# 1.2.  SpeedCore Walls
-# Asmin = rho_min *t_sc *Lw
-# Asmax = rho_max *t_sc *Lw
-# if As < Asmin: 
-#     print("Area of Steel is less than minimum for Composite Walls!!!")
-# elif As > Asmax:
-#     print("Area of Steel is greater than maximum for Composite Walls!!!")
-
-
-# 2. Check Plate Slenderness Ratios
-"""'''''''''''''''''''''''''''''"""
-# 1.1.  Coupling Beams
-# lambdaP_fCB     = 2.37 *(Es /Ry /Fy) **0.5
-# lambdaP_wCB     = 2.66 *(Es /Ry /Fy) **0.5
-
-# b_cCB           = b_CB -2 *t_pwCB
-# lambda_fCB      = b_cCB /t_pfCB
-
-# h_cCB           = h_CB -2 *t_pfCB
-# lambda_wCB      = h_cCB /t_pwCB
-
-# if lambda_fCB/lambdaP_fCB > 1.0: 
-#     print(f"Coupling Beam Flange Plate Slenderness OVERRATED!!!\n===>>>Lam_fCB/LamP_fCB = {lambda_fCB/lambdaP_fCB:.2f}")
-# if lambda_wCB/lambdaP_wCB > 1.0: 
-#     print(   f"Coupling Beam Web Plate Slenderness OVERRATED!!!\n===>>>Lam_wCB/LamP_wCB = {lambda_wCB/lambdaP_wCB:.2f}")
-
-# 1.2.  SpeedCore Walls
-# lambda_btie = btie/tw
-# lambdaP_btie= 1.05 *(Es /Ry /Fy) **0.5
-# lambdaP_btieTop = 1.2 *(Es /Fy) **0.5
-
-# if lambda_btie/lambdaP_btie > 1.0:    
-#     print(f"Wall Faceplate Slenderness in flexural yielding zone not CHECKED!!!\n===>>>Lam_fW/LamP_fW = {lambda_btie/lambdaP_btie:.2f}") 
-# if lambda_btie/lambdaP_btieTop > 1.0: 
-#     print(f"Wall Faceplate Slenderness above flexural yielding zone not CHECKED!!!\n===>>>Lam_fW/LamP_fW = {lambda_btie/lambdaP_btieTop:.2f}") 
-
-# Alpha       = 1.7 *(t_sc /tw -2) *(tw /dtie) **4
-# lambdaP_tbs = (Es /(2 *Alpha +1)) **0.5
-# lambda_tbs  = Stie /tw
-
-# if lambda_tbs/lambdaP_tbs > 1.0: 
-#     print(f"Wall Tie bar spacing not CHECKED!!!\n===>>>Lam_tbs/LamP_tbs = {lambda_tbs/lambdaP_tbs:.2f}") 
-
-
-# 3. Check Shear Criticality of Coupling Beams
-"""''''''''''''''''''''''''''''''''''''''''"""
-# t_cCB   = b_CB -2 *t_pwCB
-
-# C_CBexp = ((2 *t_pwCB *Ry *Fy*h_CB  +0.85 *Rc *fpc *t_cCB *t_pfCB)/
-#            (4 *t_pwCB *Ry *Fy       +0.85 *Rc *fpc *t_cCB))
-# print(f"C_CBexp = {C_CBexp *1000:.1f} mm")
-# C_1exp  = (b_CB -2 *t_pwCB) *t_pfCB *Ry *Fy
-# C_2exp  = 2 *t_pwCB *C_CBexp *Ry *Fy
-# C_3exp  = 0.85 *Rc *fpc *t_cCB *(C_CBexp -t_pfCB)
-# T_1exp  = (b_CB -2 *t_pwCB) *t_pfCB *Ry *Fy
-# T_2exp  = 2 *t_pwCB *(h_CB -C_CBexp) *Ry *Fy
-# M_exp = (C_1exp *(C_CBexp -t_pfCB /2) +
-#             C_2exp *(C_CBexp /2) +
-#             C_3exp *((C_CBexp -t_pfCB) /2) +
-#             T_1exp *(h_CB -C_CBexp -t_pfCB /2) +
-#             T_2exp *((h_CB -C_CBexp) /2))
-# V_exp    = 0.6 *Ry *Fy *Asw_CB +0.06 *Ac_CB *(Rc *fpc /MPa) **0.5 #Take care NOT to put fpc in other units except MPa
-
-# print(f"M_exp = {M_exp /1000:.1f} kN.m")
-# print(f"V_exp = {V_exp /1000:.1f} kN")
-
-# # Check Flexure-Criticality Condition
-# if V_exp *L_CB /M_exp >= 2.4: 
-#     print("Flexure-Criticality Confirmed!")
 
 numSign = 65
 #||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||
